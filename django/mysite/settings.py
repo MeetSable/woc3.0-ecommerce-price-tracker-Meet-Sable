c9d88303bad6a5126d9012d03fabe7b73207b9da
MeetSable <meetsable@gmail.com>
2021-01-31 10:40:49 +0530

    modified:   settings.py

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xaz!e6a-5)cs&thqbrldp#j@op@17&=kij6020@54c6x#r(%0%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 
 
 ALLOWED_HOSTS = []
 
